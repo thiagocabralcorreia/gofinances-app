64b984ffc0f667bfaf6c4210d63a64c82b92cabf
thiagocabralcorreia <thiagocabralcorreia@gmail.com>
2022-09-18 17:21:29 -0300

    Add TransactionCard component and dashboard transaction list

---
+++ src/utils/categories/index.tsx
@@ -0,0 +1,13 @@
+export const categories = [
+  {
+    key: "purchases",
+    name: "Purchases",
+    icon: "shopping-bag",
+    color: "#5636D3",
+  },
+  { key: "food", name: "Food", icon: "coffee", color: "#FF872C" },
+  { key: "salary", name: "Salary", icon: "dollar-sign", color: "#12A454" },
+  { key: "car", name: "Car", icon: "crosshair", color: "#E83F5B" },
+  { key: "leisure", name: "Leisure", icon: "heart", color: "#26195C" },
+  { key: "studies", name: "Studies", icon: "book", color: "#9C001A" },
+];
